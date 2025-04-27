09a5ee56029da75b0d41b11f7581bb8146568f4a
bugkwuynx <hngo_2028@depauw.edu>
2025-04-26 23:54:39 -0400

    update

--- backend/routes/chatBot.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { chatBot } = require('../controllers/chatBot');
-
-router.post('/chat', chatBot);
-
-module.exports = router;
--- backend/routes/sendEmailRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { sendEmail } = require('../controllers/sendEmail');
-
-router.post('/sendEmail', sendEmail);
-
-module.exports = router;
